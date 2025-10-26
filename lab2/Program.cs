d6ace0cd178dd916cfd0618a1f7a590e131f94f9
71366 <71366@KM205-16>
2025-10-26 12:28:55 +0100

    Dodaj pliki projektów.

---
+++ lab2/Program.cs
@@ -0,0 +1,9 @@
+﻿using lab2;
+
+BankAccount bankAccount = new BankAccount("Nikodem","Szubierski", 1m);
+bankAccount.view();
+
+bankAccount.wplata(100);
+bankAccount.view();
+bankAccount.wyplata(50);
+bankAccount.view();
